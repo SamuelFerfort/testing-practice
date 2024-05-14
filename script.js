385fa91d48047ddc46fc5b2e3823aaca6c1c1271
Samuel <samuferfort@gmail.com>
2024-05-14 06:30:20 +0200

    add analyzeArray and test it

--- script.js
+++ script.js
@@ -19,3 +19,32 @@ export function calc() {
     divide: (a, b) => a / b,
   };
 }
+
+export function caesar(str, shift) {
+  let output = "";
+  let a = "a".charCodeAt(0) - 1;
+  let A = "A".charCodeAt(0) - 1;
+  for (const char of str) {
+    let code = char.charCodeAt(0) + shift;
+    if (char === "z") code = a + shift;
+    if (char === "Z") code = A + shift;
+    if (!/[a-zA-Z]/.test(char)) {
+      output += char;
+      continue;
+    }
+    output += String.fromCharCode(code);
+  }
+
+  return output;
+}
+
+export function analyzeArray(arr) {
+  if (arr.some((n) => typeof n !== "number")) return null;
+
+  return {
+    average: arr.reduce((total, value) => total + value, 0) / arr.length,
+    min: Math.min(...arr),
+    max: Math.max(...arr),
+    length: arr.length,
+  };
+}
--- script.test.js
+++ script.test.js
@@ -1,4 +1,10 @@
-import { capitalize, reverseString, calc } from "./script";
+import {
+  capitalize,
+  reverseString,
+  calc,
+  caesar,
+  analyzeArray,
+} from "./script";
 
 test("Capitalize", () => {
   expect(capitalize("hello")).toBe("Hello");
@@ -33,4 +39,28 @@ describe("Calculator", () => {
   });
 });
 
+test("Caesar cypher z check", () => {
+  expect(caesar("az", 1)).toBe("ba");
+});
+
+test("Caesar cypher caps", () => {
+  expect(caesar("BZ", 3)).toBe("EC");
+});
+
+test("Caesar cypher caps", () => {
+  expect(caesar("hello,world!", 3)).toBe("khoor,zruog!");
+});
+
+test("analyzeArray", () => {
+  expect(analyzeArray([1, 8, 3, 4, 2, 6])).toEqual({
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6,
+  });
+});
+
+test("analyzeArray", () => {
+  expect(analyzeArray([1, 8, 3, 4, 2, "A"])).toBeNull()
+});
 
